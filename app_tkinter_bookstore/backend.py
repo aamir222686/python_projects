1015a6e7db2bac5acf73ec60bf63b571d82065c9
aamir222686 <aamir222686@yahoo.com>
2020-07-25 18:31:18 +0530

    clean code with classes added
    Removed files not needed. Running the front end file will create a database automatically.

--- app_tkinter_bookstore/backend.py
+++ app_tkinter_bookstore/backend.py
@@ -1,54 +1,43 @@
-# -*- coding: utf-8 -*-
 """
 Backend using the Sqlite3 package
 """
 
 import sqlite3
 
-def connect():
-    conn = sqlite3.connect("bookstore.db")
-    cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS store (id INTEGER PRIMARY KEY, title text, author text, year integer, isbn integer)")
-    conn.commit()
-    conn.close()
+class Database:
     
-connect() #creates a new db for first time run
-
-def insert(title, author, year, isbn):
-    conn = sqlite3.connect("bookstore.db")
-    cur = conn.cursor()
-    cur.execute("INSERT INTO store VALUES (NULL, ?,?,?,?)", (title, author, year, isbn))
-    conn.commit()
-    conn.close()
-
-def view_all():
-    conn = sqlite3.connect("bookstore.db")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM store")
-    rows = cur.fetchall()
-    conn.commit()
-    conn.close()
-    return rows
+    def __init__(self, db):
+        self.conn = sqlite3.connect(db)
+        self.cur = self.conn.cursor()
+        self.cur.execute("CREATE TABLE IF NOT EXISTS store (id INTEGER PRIMARY KEY, title text, author text, year integer, isbn integer)")
+        self.conn.commit()
     
-def search(title="", author="", year="", isbn=""):
-    conn = sqlite3.connect("bookstore.db")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM store WHERE title=? OR author=? OR year=? OR isbn=?",(title, author, year, isbn))
-    rows = cur.fetchall()
-    conn.commit()
-    conn.close()
-    return rows
-
-def delete(id):
-    conn = sqlite3.connect("bookstore.db")
-    cur = conn.cursor()
-    cur.execute("DELETE FROM store WHERE id=?", (id,))
-    conn.commit()
-    conn.close()
-
-def update(id, title, author, year, isbn):
-    conn = sqlite3.connect("bookstore.db")
-    cur = conn.cursor()
-    cur.execute("UPDATE store SET title=?, author=?, year=?, isbn=? WHERE id=?", (title, author, year, isbn, id))
-    conn.commit()
-    conn.close()
+    def insert(self, title, author, year, isbn):
+        self.cur.execute("INSERT INTO store VALUES (NULL, ?,?,?,?)", (title, author, year, isbn))
+        self.conn.commit()
+    
+    def view_all(self):
+        self.cur.execute("SELECT * FROM store")
+        rows = self.cur.fetchall()
+        self.conn.commit()
+        return rows
+        
+    def search(self, title="", author="", year="", isbn=""):
+        self.cur.execute("SELECT * FROM store WHERE title=? OR author=? OR year=? OR isbn=?",(title, author, year, isbn))
+        rows = self.cur.fetchall()
+        self.conn.commit()
+        return rows
+    
+    def delete(self, id):
+        self.cur.execute("DELETE FROM store WHERE id=?", (id,))
+        self.conn.commit()
+    
+    def update(self, id, title, author, year, isbn):
+        self.cur.execute("UPDATE store SET title=?, author=?, year=?, isbn=? WHERE id=?", (title, author, year, isbn, id))
+        self.conn.commit()
+        
+    def close_conn(self):
+        self.conn.close()
+        
+    def __del__(self):
+        self.conn.close()
